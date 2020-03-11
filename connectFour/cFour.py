6593a4b3ba5b47f95bb41123accab8baf5a665c5
Logan Voruz <lvoruz@msn.com>
2020-03-11 16:35:55 -0700

    Add project files.

---
+++ connectFour/cFour.py
@@ -0,0 +1,73 @@
+class cFour():
+    def __init__(self):
+        self.grid = []
+        for i in range(6):
+            row = []
+            for j in range(7):
+                row.append('O')
+            self.grid.append(row)
+        self.score = {'Red': 0, 'Black': 0, 'Winner': ''}
+        self.amounts = [0,0,0,0,0,0,0]
+
+    def put(self, player, column):
+        if self.amounts[column] == 6:
+            return 0
+        if player == 'black':
+            self.grid[5 - self.amounts[column]][column] = 'B'
+        elif player == 'red':
+            self.grid[5 - self.amounts[column]]s[column] = 'R'
+        else:
+            print('error: invalid player code')
+            return -1
+        self.amounts[column] += 1
+        return 1
+
+    def display(self):
+        for i in range(6):
+            for j in range(7):
+                print(self.grid[i][j], end = ' ')
+            print()
+        return
+
+    def win(self):
+        for i in range(6):
+            for j in range(7):
+                if self.grid[i][j] == 'B':
+                    w = self.checkRC(i, j, 'B')
+                    if w == True:
+                        return 'Black'
+                    w = self.checkDiag(i, j, 'B')
+                    if w == True:
+                        return 'Black'
+                elif self.grid[i][j] == 'R':
+                    w = self.checkRC(i, j, 'R')
+                    if w == True:
+                        return 'Red'
+                    w = self.checkDiag(i, j, 'R')
+                    if w == True:
+                        return 'Red'
+                else:
+                    continue
+        return None
+
+    def checkRC(self, i, j, player):
+        inRow = 0
+        for column in range(7):
+            if self.grid[i][column] == player:
+                inRow += 1
+            else:
+                inRow = 0
+            if inRow >= 4:
+                return True
+        inColumn = 0
+        for row in range(6):
+            if self.grid[row][j] == player:
+                inColumn += 1
+            else:
+                inColumn = 0
+            if inColumn >= 4:
+                return True
+        return False
+
+    def checkDiag(self, i, j, player):
+        return False
---
+++ connectFour/connectFour.py
@@ -0,0 +1,28 @@
+import sys
+import cFour
+
+def main():
+    if len(sys.argv) > 3:
+        print('usage: python3 connectFour.py [String] [String] || python3 connectFour.py')
+        return
+    if len(sys.argv) > 1 and len(sys.argv) < 3:
+        print('usage: python3 connectFour.py [String] [String] || python3 connectFour.py')
+        return
+    c = cFour.cFour()
+    turn = 1
+    while True:
+        c.display()
+        i = int(input("Select column to place piece: "))
+        player = ''
+        if turn%2 == 1:
+            player = 'black'
+        else:
+            player = 'red'
+        c.put(player, i)
+        if c.win() == True:
+            return
+        turn += 1
+
+    
+
+main()
